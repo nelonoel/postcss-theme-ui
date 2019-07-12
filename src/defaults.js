96a057bf6dcb05f1504645d3d3c629ebbde907b1
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2019-07-12 19:31:38 +0800

    Support negative space values

--- src/defaults.js
+++ src/defaults.js
@@ -2,5 +2,7 @@
 export default {
 	breakpoints: ['40em', '52em', '64em'],
 	fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
-	space: [0, 4, 8, 16, 32, 64, 128, 256, 512]
+	space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
+	sizes: [],
+	radii: []
 }
--- src/index.js
+++ src/index.js
@@ -1,25 +1,49 @@
-import postcss from 'postcss';
-import defaults from './defaults';
-import mapping from './mapping';
+import postcss from "postcss"
+import defaults from "./defaults"
+import mapping from "./mapping"
 
-import get from 'lodash/get';
+import get from "lodash/get"
 
-export default postcss.plugin('postcss-theme-ui', (options = {}) => {
-	const theme = {...defaults, ...options}
+const convertIntToPx = scale => {
+	for (let [k, v] of Object.entries(scale)) {
+		if (Number.isInteger(v) && v !== 0) {
+			scale[k] = `${v}px`
+		}
+	}
+}
+
+const addNegativeCounterpart = scale => {
+	for (let [k, v] of Object.entries(scale)) {
+		k = parseInt(k)
+		if (Number.isInteger(k) && k > 0) {
+			scale[-k] = `-${v}`
+		}
+	}
+}
+
+export default postcss.plugin("postcss-theme-ui", (options = {}) => {
+	const theme = { ...defaults, ...options }
+
+	convertIntToPx(theme.breakpoints)
+	convertIntToPx(theme.fontSizes)
+	convertIntToPx(theme.radii)
+	convertIntToPx(theme.sizes)
+	convertIntToPx(theme.space)
+	addNegativeCounterpart(theme.space)
 
 	const props = Object.keys(mapping)
 
-	return (css) => {
+	return css => {
 		css.walkRules(rule => {
 			rule.walkDecls(decl => {
-				if(props.indexOf(decl.prop) > -1) {
-					const rawValues = decl.value.trim().split(' ')
-					decl.value = rawValues.map(v => {
-						const themeValue = get(theme, `${mapping[decl.prop]}.${v}`, v)
-						return Number.isInteger(themeValue) && themeValue !== 0 ? `${themeValue}px` : themeValue
-					}).join(' ')
+				if (props.indexOf(decl.prop) > -1) {
+					decl.value = decl.value
+						.trim()
+						.split(" ")
+						.map(v => get(theme, `${mapping[decl.prop]}.${v}`, v))
+						.join(" ")
 				}
 			})
 		})
-	};
-});
+	}
+})
