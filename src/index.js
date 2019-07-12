e70830bb026cf27b2d062e7bff26a53d4dd0dee2
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2019-07-12 15:18:33 +0800

    Convert theme keys to theme-values

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import postcss from 'postcss';
+import defaults from './defaults';
+import mapping from './mapping';
+
+import get from 'lodash/get';
+
+export default postcss.plugin('postcss-theme-ui', (options = { theme: {} }) => {
+	const theme = {...defaults, ...options.theme}
+
+	const props = Object.keys(mapping)
+
+	return (css) => {
+		css.walkRules(rule => {
+			rule.walkDecls(decl => {
+				if(props.indexOf(decl.prop) > -1) {
+					const rawValues = decl.value.trim().split(' ')
+					decl.value = rawValues.map(v => {
+						const themeValue = get(theme, `${mapping[decl.prop]}.${v}`, v)
+						return Number.isInteger(themeValue) && themeValue !== 0 ? `${themeValue}px` : themeValue
+					}).join(' ')
+				}
+			})
+		})
+	};
+});
