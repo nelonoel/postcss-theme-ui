f640af6cca1c2898c152abef33188ae83b314398
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2019-07-12 21:31:44 +0800

    Add additional tests

--- .tape.js
+++ .tape.js
@@ -1,12 +1,20 @@
-var theme = require('./test/theme.js')
+var theme = require("./test/theme.js");
 
 module.exports = {
-	'basic': {
-		message: 'supports basic value retrieval',
+	basic: {
+		message: "supports basic value retrieval",
 		options: theme
 	},
-	'negative-values': {
-		message: 'supports negative values',
+	"negative-values": {
+		message: "supports negative values",
+		options: theme
+	},
+	"shorthand-props": {
+		message: "supports CSS shorthand props",
+		options: theme
+	},
+	"array-as-props": {
+		message: "supports array as props",
 		options: theme
 	}
 };
--- test/theme.js
+++ test/theme.js
@@ -1,16 +1,16 @@
 module.exports = {
 	borders: ["none", "solid 1px", "solid 2px"],
 	colors: {
-		text: '#000',
-		background: '#fff',
-		primary: '#07c',
-		secondary: '#05a',
-		accent: '#609',
-		muted: '#f6f6f6f'
+		text: "#000",
+		background: "#fff",
+		primary: "#07c",
+		secondary: "#05a",
+		accent: "#609",
+		muted: "#f6f6f6f"
 	},
 	fonts: {
 		sans: '"IBM Plex Sans", sans-serif',
-		serif: '"IBM Plex Serif", serif',
+		serif: '"IBM Plex Serif", serif'
 	},
 	fontSizes: {
 		"0": "1rem",
@@ -19,21 +19,25 @@ module.exports = {
 		"3": "2.5rem",
 		"4": "3.5rem",
 		"-1": "0.75rem",
-		"base": 16
+		base: 16
 	},
 	fontWeights: {
 		medium: 500,
 		normal: 400,
-		bold: 700,
+		bold: 700
 	},
 	letterSpacings: {
 		normal: "normal",
 		caps: "0.125rem",
-		tight: "-0.0625rem",
+		tight: "-0.0625rem"
 	},
-  lineHeights: [1, 1.25, 1.5, 2],
-  radii: [0, 4, 8, 16, 9999, "50%"],
-  shadows: ['none', '0 1px 1px rgba(0, 0, 0, 0.05)', '0 1px 3px rgba(0, 0, 0, 0.1)'],
-  sizes: ["auto", "48rem", "64rem"],
-  space: [0, 4, 8, 16, 32, 64, 128, 256, 512]
-}
+	lineHeights: [1, 1.25, 1.5, 2],
+	radii: [0, 4, 8, 16, 9999, "50%"],
+	shadows: [
+		"none",
+		"0 1px 1px rgba(0, 0, 0, 0.05)",
+		"0 1px 3px rgba(0, 0, 0, 0.1)"
+	],
+	sizes: ["auto", "48rem", "64rem"],
+	space: [0, 4, 8, 16, 32, 64, 128, 256, 512]
+};
