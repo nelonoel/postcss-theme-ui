5d94953d2fa164bf6e959cc35a1bb66162179817
Aaron Noel De Leon <aaronnoeldeleon@gmail.com>
2019-07-12 15:17:02 +0800

    Add theme defaults

---
+++ src/defaults.js
@@ -0,0 +1,6 @@
+// Reference: https://styled-system.com/api#defaults
+export default {
+	breakpoints: ['40em', '52em', '64em'],
+	fontSizes: [12, 14, 16, 20, 24, 32, 48, 64, 72],
+	space: [0, 4, 8, 16, 32, 64, 128, 256, 512]
+}
---
+++ src/mapping.js
@@ -0,0 +1,50 @@
+// Reference: https://styled-system.com/table/
+export default {
+	'margin': 'space',
+	'margin-top': 'space',
+	'margin-right': 'space',
+	'margin-bottom': 'space',
+	'margin-left': 'space',
+
+	'padding': 'space',
+	'padding-top': 'space',
+	'padding-right': 'space',
+	'padding-bottom': 'space',
+	'padding-left': 'space',
+
+	'grid-gap': 'space',
+	'grid-row-gap': 'space',
+	'grid-column-gap': 'space',
+
+	'color': 'colors',
+	'background-color': 'colors',
+	'border-color': 'colors',
+
+	'font-family': 'fonts',
+	'font-size': 'fontSizes',
+	'font-weight': 'fontWeights',
+	'line-height': 'lineHeights',
+	'letter-spacing': 'letterSpacings',
+
+	'width': 'sizes',
+	'height': 'sizes',
+	'min-width': 'sizes',
+	'max-width': 'sizes',
+	'min-height': 'sizes',
+	'max-height':	'sizes',
+
+	'border': 'borders',
+	'border-top': 'borders',
+	'border-right': 'borders',
+	'border-bottom': 'borders',
+	'border-left': 'borders',
+	'border-width': 'borderWidths',
+	'border-style': 'borderStyles',
+	'border-radius': 'radii',
+
+	'box-shadow': 'shadows',
+	'text-shadow': 'shadows',
+
+	'transition': 'transitions',
+	'z-index': 'zIndices'
+};
